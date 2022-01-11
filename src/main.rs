beaa2c5091f05f5ecdac65d4e98aeb79291da297
Cezar Halmagean <cezar@mixandgo.com>
2022-01-11 14:23:03 +0200

    Documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+//! This is where we put the documentation for our crate.
+//! You can find this text in `src/main.rs`.
+
+/// This text serves as documentation for the `myfunc` function.
+///
+/// # Examples
+/// ```
+///     assert_eq!(myfunc(), false)
+/// ```
+pub fn myfunc() -> bool {
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    myfunc();
 }
